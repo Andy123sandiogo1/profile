f64f81abac4f0e42cd4558acf7637e88f7853949
Andy <sandiogo@email.com>
2018-10-29 06:51:41 -0700

    bid statement

---
+++ Others/ALL_Questions.php
@@ -0,0 +1,164 @@
+-------------------------------------------------------
+***********************Questions***********************
+-------------------------------------------------------
+
+--------------------------------- CSS -------------------------------
+
+- What's your preferred methodology and/or framework for writing/organizing your CSS? Why?
+	In PHP, I like Laravel and CodeIgniter, Yii.
+Recently, I developed news app using IONIC (Angular 6). In that project, I used SASS using node module.
+node-sass....
+
+Using this module, sass compiles into css automatically.
+- Describe how accessibility and the concept of progressive enhancement work together and how they fit into your work?
+ 	1. I always love to learn someth ing new in my fields. I am always ready for challenges and i can't take rest until my work is done
+ 	2. I keep myself updated with latest techniques in this field and make sure any project I take up should meet the quality along with the fact that my work should be compatible on modern browsers and also on the basic one's.
+- What are your thoughts on whether all pages should look and behave the same in every browser?
+	1. I wondered is this position for UI/UX or front end developing, I mean are you looking for Ui designer, or you want ui deigner to do the developing at the same time.
+		
+	2. 
+- Do you rely heavily on jQuery and/or Bootstrap for front-end development? Why or why not?
+	1. It depends on the client's requirement for designin. You can check the design :on addition we are much aware of crypto space we have build front end template also : http://themes.webiots.com/coin/
+	User Avatar http://themes.webiots.com/coin/.
+	2. Yes, I use jQuery and Bootstrap to do the work in fast and efficient manner. Writing one line is better than coding 10-15 to keep the site light weighted.
+	3. No because i am a Graphic and UI/UX designer i mean i can design mobile apps, web apps and websites by using graphic tools very perfectly
+- Describe your experience and level of comfort working with front-end code in Rails with views and ERB.
+	I have an excellent experience in web crawling, scraping using phantomjs, slimerjs and casperjs, and I also have developing website/app using.
+------------------------------- NORMAL QEUSTION -------------------------------------
+- Do you have any questions about the job description?
+ I have one question. which of skills do you need?
+- Why do you think you are a good fit for this particular project?
+ With my experience, I think I'm perfectly fit for this project and my budget is also negotiable since I'm a most experienced freelancer.
+ I'm in love of RoR and favor it among all other frontend MVC frameworks
+- How many hours can you dedicate to this project per week?
+I can work for 30+ hours per week.
+
+- Which of the required job skills do you feel you are strongest at?
+I have an extensive experience building and interacting with RESTful API's, data modeling and project management in Laravel and React.
+I prefer the Laravel 5.1+.
+
+- Why did you apply to this particular job?
+Because I am a programer to listen to all my client's requirement and provide elegant but manageable solutions that are designed for your unique situation.
+
+- Why do you think you are a good fit for this particular project?
+Because I am a programer to listen to all my client's requirement and provide elegant but manageable solutions that are designed for your unique situation,
+I think I am a good fit for this particular job.
+
+---------------------------------***REACT***-----------------------------------------
+- Please describe your experience with react development
+	I used react, immutable, webpack, typescript so that I have developed the webpages such as www.corva.ai, and wanted.com. 
+	About code sharing, sorry I just can't share it publicly.
+- Please describe your experience with Material Design.
+	I have an excellent experience of developing the wepages with Material Design. I have a solid understanding of Bootstrap, CSS, Saas, and 
+- How many hours are you available to work on this project?
+	I can work on this project for a long time ASAP.
+- Can you verify your eligibility to work for hire in the United States?
+	Yes, I can.
+- Are you available to start immediately(tomorrow)?
+Sure. I'm available tomorrow for interview and can start immediately.
+
+- Are you available to have 2 interviews tomorrow(one with me, one with client)?
+Sure, let's coordinate the time if possible.
+
+- What past project or job have you had that is most like this one and why?(Github & Link)
+www.corva.ai - we used react.js, immutable, webpack, typescript
+About code sharing, sorry I just can't share it publicly
+
+------------------------------ Ruby on Rails --------------------------------------------
+
+- Do you have any questions about this project for me?
+I have no specific questions as you posted detailed description. I'm clear now.
+- If selected, when could you start on this project?
+I'm able to start in this week once we agreed to work on
+- Can you share 2-3 code samples?
+Sure, I can walk you through the source code of previous projects in github.
+
+- Are you a full time freelancer with availability during the day to hop on status calls?
+I'd be available any time during day or night. except weekends ;)
+
+- Do you have any javascript pages that you have created in the past that you want to share with me?
+Here, I'm sending JS snippet to parse CSV :
+function CSVToArray( strData, strDelimiter ){
+strDelimiter = (strDelimiter || ",");
+
+- Have you built anything using the Facebook API? If so, what?
+In past projects, I've integrated Facebook auth, graph apis.
+
+- Have you built anything using the Instagram API? If so, what?
+No, I haven't used instagram api but it shouldn't be a problem for me to use it in your project. It'd take nothing more than quickly covering couple of their api guides.
+
+- Have you built anything using the Twitter API? If so, what?
+Using Twitter api, I also have integreated authentication, ads api and messaging.
+
+- Have you built anything using the LiveChat API? If so, what?
+No, I haven't used livechat api.
+
+- Do you utilize TDD?
+Yes, I'm oriented in test or behavior driven development and write clean & easy to maintain code
+------------------------------- SQL ------------------------------------------------------
+
+- I prefer a US-based person for easier time zone communication. I'm in California (PST).
+I'm from Livermore CA!
+
+- What past project or job have you had that is most like this one and why?
+Saas platform that I've developed: www.corva.ai
+It's using react in frontend and RESTful backend apis.
+
+- What questions do you have about the project?
+What are you using to manage issues and what's the workflow ? 
+Do you have any project management platform in use like jira or PT ?
+
+- How many hours per week are you able to allocate to this project?
+Around 30 hours per week and I think it's good enough for maintenance. Also this can be discussed if needed more.
+
+------------------------ Javascript -------------------------------------------------------------
+
+- Tell us about a project where you decided to use ngrx, how did it go?
+	When it comes to use Redux, we need a store sth like ngrx. There're maybe other solutions but mainly Ngrx is a solution for you. mostly recommended
+
+- What is a difference between Ngrx and RxJS?
+	well.. Ngrx implements redux pattern using rxjs observables so it's kinda coupled.
+
+- What is Babel and how it helps with Node.js development?
+	It's js transpiler and isn't really used with node.js.
+
+
+------------------------ FullStack ---------------------------------------------------
+
+- How many years have you worked with PHP ?
+I have 5 years of commercial experience and 8+ years of experience in PHP.
+
+- What timezone are you in ?
+I'm in PST, Livermore CA.
+
+---------------
+
+---------------
+- Do you represent an agency?
+- We are only interested in working with individual developers.
+Have you used any crypto-currency wallets in the past?
+
+------------------ HTML ---------------------------
+
+-----------------React and Angular ------------------
+- Why do you think you are a good fit for this particular project?
+With my experience, I think I'm perfectly fit for this project and my budget is also negotiable since I'm a most experienced freelancer.
+
+I'm in love of Angular and React and favor it among all other frontend MVC frameworks
+
+- Are you familiar with coding in AngularJS, React.js, HTML, CSS, Bootstrap?
+Yes, I am. Over the last 5 years I've developed & participated 5+ projects using CSS 2+ in React.js, AngularJS, RoR, etc..
+
+- What challenging part of this job are you most experienced in?
+I'm a web developer with 5+ years of experience in PHP, Laravel, Vue, etc. 
+I think that it is important to have a solid understanding of server and security configurations.
+- Which part of this project do you think will take the most time?
+As I think, it will take the most time to design this project structure.
+--------------------------------------
+- Do you have any questions about the job description?
+	How i will work and is there any documentation of the requirement?
+- Please have examples of previous work you can showcase
+
+- What challenging part of this job are you most experienced in?
+
+- Which part of this project do you think will take the most time?
---
+++ profile
@@ -0,0 +1,74 @@
+profile
+-----------------------
+Veteran Web Developer.
+
+-----------------------
+
+
+----- my upwork profile ---------
+
+Full-Stack Web Developer
+
+I'm a full-stack Web Developer with 6+ years of experience in PHP, JavaScript, Sql, developing and constructing web sites.
+
+Technologies:
+-Frontend: HTML5, JavaScript, JQuery, Angular, React
+-Backend: PHP, Laravel, Node.js
+-Database: MySQL, PostgreSQL
+-version Controls: Git, Bitbucket
+
+I have been developing the webpages for a long time, and have a solid understanding of server configuration, reliable plugins, security configuration and backup solution.
+
+I always take the time to listen to all my client's requiment and provide elegant and manageable solutions that are designed for your unique situation.
+
+As Upwork starter, my initial target is to get a rising-talent. 
+I'm a hard working person, and always focused at the result and quality of my work.
+I'll work around the clock untill my clients are satisfied.
+
+Thank you for reading my profile.
+
+
+
+I have proud portfolio projects as well. 
+ -smartassetmanagers.com
+ -ddkoin.com
+ -tessrnet.com
+ 
+-------------------------------------------------
+|Both my work and interrest in learning get mostly native development experience on this |platform.
+|
+|I have also worked on other platforms/languages for my past works, sucha as: C++, C#, ASP.NET |Core, PHP and JavaScript.
+|
+|I am looking for clients who want to give me a chance to show my skills since I need some good |review on this site be sure that I will put maximum effort to deliver your project achieving |success and meeting the expectations.
+--------------------------------------------------
+skills: HTML5, JavaScript, CSS, Bootstrap3+, PHP7.0+,
+--------------------------------------------------
+- Front-end: HTML5, Javascript, CSS, Bootstrap, Angular
+- Back-end: PHP, Laravel,
+- SQL: MySql
+------------------------ Portfolio 1 http://smartassetmanagers.com/----------------------
+
+I developed to focus the UX/UI, Sass and Response UI.
+
+Responsibility:
+- UX/UI
+- Animation Effect
+- latest Bootstrap
+- Response UI
+- eCommerce
+------------------------- Portfolio 2 https://tessrnet.com---------------------
+Verify Personal Info
+
+We applied various verifiy such as phone, email, personal verify.
+
+Responsibility:
+- eCommerce
+- verify personal information
+
+
+----------------------- ADD education -----------------
+
+- school: Federal University of Maranhao
+- degree: bachelor's degree
+- computer science.
+
